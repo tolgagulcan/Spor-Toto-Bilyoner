4eb1a467821c90a34fadcbb1575a758e3fb19764
Tolga GULCAN <tolgagulcan@hotmail.com>
2017-03-09 22:53:05 +0300

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.w1 = new System.Windows.Forms.WebBrowser();
+            this.baslat = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // w1
+            // 
+            this.w1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.w1.Location = new System.Drawing.Point(0, 0);
+            this.w1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.w1.Name = "w1";
+            this.w1.ScriptErrorsSuppressed = true;
+            this.w1.Size = new System.Drawing.Size(649, 535);
+            this.w1.TabIndex = 0;
+            this.w1.Url = new System.Uri("https://www.bilyoner.com/milli-piyango/milli-piyango-bileti-al", System.UriKind.Absolute);
+            this.w1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.w1_DocumentCompleted);
+            // 
+            // baslat
+            // 
+            this.baslat.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.baslat.Location = new System.Drawing.Point(0, 0);
+            this.baslat.Name = "baslat";
+            this.baslat.Size = new System.Drawing.Size(128, 74);
+            this.baslat.TabIndex = 1;
+            this.baslat.Text = "BAŞLAT";
+            this.baslat.UseVisualStyleBackColor = true;
+            this.baslat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(283, 155);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(326, 20);
+            this.t1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 535);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.baslat);
+            this.Controls.Add(this.w1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser w1;
+        private System.Windows.Forms.Button baslat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox t1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        StringCollection tumkuponlar = new StringCollection();
+        bool a = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void w1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+
+            
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+               new System.IO.StreamReader("test.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                tumkuponlar.Add(line);
+                counter++;
+            }
+
+         file.Close();
+            
+
+          
+        }
+
+
+        public bool testet()
+        {
+
+            string dogru = "0-1-1-2-2-1-2-0-0-0-1-1-2-1-2";
+            dogru = t1.Text;
+            string[] d = dogru.Split('-');
+
+            if (d.Length!=15)
+            {
+                MessageBox.Show("HATA VAR");
+                return false;
+            }
+
+           
+
+
+            int[] sayim = new int[16] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
+
+            foreach (var item in tumkuponlar)
+            {
+                int say = 0;
+                string[] a = item.Split('-');
+
+                for (int i = 0; i < 15; i++)
+                {
+                    if (d[i]==a[i])
+                    {
+                        say++;
+                    }
+                }
+
+                sayim[say]++;
+
+
+
+            }
+
+            string stra = "";
+            for (int i = 0; i < 16; i++)
+            {
+                stra = stra+i.ToString() + "-" + sayim[i].ToString() + "&";   
+
+            }
+
+            MessageBox.Show(stra);
+
+            return false;
+
+        }
+
+        public bool play() {
+
+            
+            
+
+            if (tumkuponlar.Count==0)
+            {
+                return true;
+            }
+
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                
+                string kupon = tumkuponlar[0];
+                tumkuponlar.RemoveAt(0);
+
+                string[] a = kupon.Split('-');
+
+                for (int b = 0; b < 15; b++)
+                {
+
+                    int h = a[b].Length;
+                    for (int c = 0; c < h; c++)
+                    {
+                        string id = idgonder(i + 1, int.Parse((a[b][c]).ToString()),b+1);
+                        w1.Document.GetElementById(id).InvokeMember("click");
+                    }
+                }
+
+
+                if (tumkuponlar.Count == 0)
+                {
+                    return true;
+                }
+
+
+
+            }
+
+
+            return false;
+
+        }
+
+        public string idgonder(int kolon, int sonuc,int mac) {
+
+            string id="";
+            id = "ckid";
+
+            
+            // KOLON BELİRLEME
+            id = (kolon == 1) ? id + "A" : id;
+            id = (kolon == 2) ? id + "B" : id;
+            id = (kolon == 3) ? id + "C" : id;
+            id = (kolon == 4) ? id + "D" : id;
+
+            //
+
+
+
+            //SONUC BELİRLEME
+            id = (sonuc == 1) ? id + "Bir" : id;
+            id = (sonuc == 0) ? id + "X" : id;
+            id = (sonuc == 2) ? id + "Iki" : id;
+            //
+
+            //Mac belirleme
+            id = id + mac.ToString();
+
+            //
+            return id;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            w1.DocumentCompleted += kkk;
+            w1.Navigate("https://www.bilyoner.com/spor-toto/bahis-yap");
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void kkk(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+
+            //MessageBox.Show(e.Url.ToString());
+            //MessageBox.Show("https://www.bilyoner.com/spor-toto/bahis-yap");
+
+            
+            if (e.Url.ToString() == "https://www.bilyoner.com/spor-toto/spor-toto-onay")
+            {
+
+                string jCode = "onayla(7);";
+                w1.Document.InvokeScript("eval", new object[] { jCode });
+
+            }
+
+            if (e.Url.ToString() == "https://www.bilyoner.com/spor-toto/SporTotoPlay")
+            {
+
+                if (a) { w1.DocumentCompleted -= kkk; MessageBox.Show("Finished"); }
+                w1.Navigate("https://www.bilyoner.com/spor-toto/bahis-yap");
+
+            }
+
+            if (e.Url.ToString() == "https://www.bilyoner.com/spor-toto/bahis-yap")
+            {
+              
+                a = play();
+
+                
+
+                string jCode = "sporTotoOnay();";
+                w1.Document.InvokeScript("eval", new object[] { jCode });
+
+            }
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            testet();
+        }
+    }
+}
